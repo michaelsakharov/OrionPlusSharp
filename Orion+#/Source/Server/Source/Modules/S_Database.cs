e95ee252577c48995ee9493f33287cd90b4161ca
Michael S <acer1171090@gmail.com>
2019-08-01 19:36:43 +1000

    Server: Removed old Debug Console.Write

--- Orion+#/Source/Server/Source/Modules/S_Database.cs
+++ Orion+#/Source/Server/Source/Modules/S_Database.cs
@@ -393,7 +393,6 @@ namespace Engine
                         myXml.WriteString("Event" + i, "x", modTypes.Map[mapNum].Events[i].X.ToString());
                         myXml.WriteString("Event" + i, "y", modTypes.Map[mapNum].Events[i].Y.ToString());
                         myXml.WriteString("Event" + i, "PageCount", modTypes.Map[mapNum].Events[i].PageCount.ToString());
-                        Console.WriteLine(modTypes.Map[mapNum].Events[i].PageCount);
                     }
                     if (modTypes.Map[mapNum].Events[i].PageCount + 1 > 0)
                     {
