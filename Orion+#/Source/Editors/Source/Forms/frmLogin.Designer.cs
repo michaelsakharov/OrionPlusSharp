f491a98a9f4b7b7001d2a7fe7f92c8189770559d
Michael S <acer1171090@gmail.com>
2019-08-02 15:47:55 +1000

    Editor: Fixed Menu Buttons

--- Orion+#/Source/Editors/Source/Forms/frmLogin.Designer.cs
+++ Orion+#/Source/Editors/Source/Forms/frmLogin.Designer.cs
@@ -162,142 +162,170 @@ namespace Engine
             // 
             // btnPetEditor
             // 
-            this.btnPetEditor.Location = new System.Drawing.Point(122, 161);
+            this.btnPetEditor.BackColor = System.Drawing.Color.White;
+            this.btnPetEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnPetEditor.Location = new System.Drawing.Point(122, 158);
             this.btnPetEditor.Name = "btnPetEditor";
             this.btnPetEditor.Size = new System.Drawing.Size(112, 25);
             this.btnPetEditor.TabIndex = 54;
             this.btnPetEditor.Text = "Pet Editor";
-            this.btnPetEditor.UseVisualStyleBackColor = true;
+            this.btnPetEditor.UseVisualStyleBackColor = false;
             this.btnPetEditor.Click += new System.EventHandler(this.BtnPetEditor_Click);
             // 
             // btnAutoMapper
             // 
+            this.btnAutoMapper.BackColor = System.Drawing.Color.White;
+            this.btnAutoMapper.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnAutoMapper.Location = new System.Drawing.Point(240, 34);
             this.btnAutoMapper.Name = "btnAutoMapper";
             this.btnAutoMapper.Size = new System.Drawing.Size(112, 25);
             this.btnAutoMapper.TabIndex = 53;
             this.btnAutoMapper.Text = "Auto Mapper";
-            this.btnAutoMapper.UseVisualStyleBackColor = true;
+            this.btnAutoMapper.UseVisualStyleBackColor = false;
             this.btnAutoMapper.Click += new System.EventHandler(this.BtnAutoMapper_Click);
             // 
             // btnClassEditor
             // 
+            this.btnClassEditor.BackColor = System.Drawing.Color.White;
+            this.btnClassEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnClassEditor.Location = new System.Drawing.Point(122, 3);
             this.btnClassEditor.Name = "btnClassEditor";
             this.btnClassEditor.Size = new System.Drawing.Size(112, 25);
             this.btnClassEditor.TabIndex = 52;
             this.btnClassEditor.Text = "Class Editor";
-            this.btnClassEditor.UseVisualStyleBackColor = true;
+            this.btnClassEditor.UseVisualStyleBackColor = false;
             this.btnClassEditor.Click += new System.EventHandler(this.BtnClassEditor_Click);
             // 
             // btnRecipeEditor
             // 
+            this.btnRecipeEditor.BackColor = System.Drawing.Color.White;
+            this.btnRecipeEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnRecipeEditor.Location = new System.Drawing.Point(122, 127);
             this.btnRecipeEditor.Name = "btnRecipeEditor";
             this.btnRecipeEditor.Size = new System.Drawing.Size(112, 25);
             this.btnRecipeEditor.TabIndex = 51;
             this.btnRecipeEditor.Text = "Recipe Editor";
-            this.btnRecipeEditor.UseVisualStyleBackColor = true;
+            this.btnRecipeEditor.UseVisualStyleBackColor = false;
             this.btnRecipeEditor.Click += new System.EventHandler(this.BtnRecipeEditor_Click);
             // 
             // btnProjectiles
             // 
+            this.btnProjectiles.BackColor = System.Drawing.Color.White;
+            this.btnProjectiles.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnProjectiles.Location = new System.Drawing.Point(122, 34);
             this.btnProjectiles.Name = "btnProjectiles";
             this.btnProjectiles.Size = new System.Drawing.Size(112, 25);
             this.btnProjectiles.TabIndex = 50;
             this.btnProjectiles.Text = "Projectiles Editor";
-            this.btnProjectiles.UseVisualStyleBackColor = true;
+            this.btnProjectiles.UseVisualStyleBackColor = false;
             this.btnProjectiles.Click += new System.EventHandler(this.BtnProjectiles_Click);
             // 
             // btnQuest
             // 
+            this.btnQuest.BackColor = System.Drawing.Color.White;
+            this.btnQuest.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnQuest.Location = new System.Drawing.Point(4, 65);
             this.btnQuest.Name = "btnQuest";
             this.btnQuest.Size = new System.Drawing.Size(112, 25);
             this.btnQuest.TabIndex = 48;
             this.btnQuest.Text = "Quest Editor";
-            this.btnQuest.UseVisualStyleBackColor = true;
+            this.btnQuest.UseVisualStyleBackColor = false;
             this.btnQuest.Click += new System.EventHandler(this.BtnQuest_Click);
             // 
             // btnhouseEditor
             // 
+            this.btnhouseEditor.BackColor = System.Drawing.Color.White;
+            this.btnhouseEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnhouseEditor.Location = new System.Drawing.Point(240, 3);
             this.btnhouseEditor.Name = "btnhouseEditor";
             this.btnhouseEditor.Size = new System.Drawing.Size(112, 25);
             this.btnhouseEditor.TabIndex = 49;
             this.btnhouseEditor.Text = "Houses Editor";
-            this.btnhouseEditor.UseVisualStyleBackColor = true;
+            this.btnhouseEditor.UseVisualStyleBackColor = false;
             this.btnhouseEditor.Click += new System.EventHandler(this.BtnhouseEditor_Click);
             // 
             // btnMapEditor
             // 
+            this.btnMapEditor.BackColor = System.Drawing.Color.White;
+            this.btnMapEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnMapEditor.Location = new System.Drawing.Point(4, 3);
             this.btnMapEditor.Name = "btnMapEditor";
             this.btnMapEditor.Size = new System.Drawing.Size(112, 25);
             this.btnMapEditor.TabIndex = 41;
             this.btnMapEditor.Text = "Map Editor";
-            this.btnMapEditor.UseVisualStyleBackColor = true;
+            this.btnMapEditor.UseVisualStyleBackColor = false;
             this.btnMapEditor.Click += new System.EventHandler(this.BtnMapEditor_Click);
             // 
             // btnItemEditor
             // 
+            this.btnItemEditor.BackColor = System.Drawing.Color.White;
+            this.btnItemEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnItemEditor.Location = new System.Drawing.Point(4, 34);
             this.btnItemEditor.Name = "btnItemEditor";
             this.btnItemEditor.Size = new System.Drawing.Size(112, 25);
             this.btnItemEditor.TabIndex = 42;
             this.btnItemEditor.Text = "Item Editor";
-            this.btnItemEditor.UseVisualStyleBackColor = true;
+            this.btnItemEditor.UseVisualStyleBackColor = false;
             this.btnItemEditor.Click += new System.EventHandler(this.BtnItemEditor_Click);
             // 
             // btnResourceEditor
             // 
+            this.btnResourceEditor.BackColor = System.Drawing.Color.White;
+            this.btnResourceEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnResourceEditor.Location = new System.Drawing.Point(122, 65);
             this.btnResourceEditor.Name = "btnResourceEditor";
             this.btnResourceEditor.Size = new System.Drawing.Size(112, 25);
             this.btnResourceEditor.TabIndex = 43;
             this.btnResourceEditor.Text = "Resource Editor";
-            this.btnResourceEditor.UseVisualStyleBackColor = true;
+            this.btnResourceEditor.UseVisualStyleBackColor = false;
             this.btnResourceEditor.Click += new System.EventHandler(this.BtnResourceEditor_Click);
             // 
             // btnNPCEditor
             // 
+            this.btnNPCEditor.BackColor = System.Drawing.Color.White;
+            this.btnNPCEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnNPCEditor.Location = new System.Drawing.Point(4, 96);
             this.btnNPCEditor.Name = "btnNPCEditor";
             this.btnNPCEditor.Size = new System.Drawing.Size(112, 25);
             this.btnNPCEditor.TabIndex = 44;
             this.btnNPCEditor.Text = "NPC Editor";
-            this.btnNPCEditor.UseVisualStyleBackColor = true;
+            this.btnNPCEditor.UseVisualStyleBackColor = false;
             this.btnNPCEditor.Click += new System.EventHandler(this.BtnNPCEditor_Click);
             // 
             // btnSkillEditor
             // 
+            this.btnSkillEditor.BackColor = System.Drawing.Color.White;
+            this.btnSkillEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnSkillEditor.Location = new System.Drawing.Point(3, 127);
             this.btnSkillEditor.Name = "btnSkillEditor";
             this.btnSkillEditor.Size = new System.Drawing.Size(112, 25);
             this.btnSkillEditor.TabIndex = 45;
             this.btnSkillEditor.Text = "Skill Editor";
-            this.btnSkillEditor.UseVisualStyleBackColor = true;
+            this.btnSkillEditor.UseVisualStyleBackColor = false;
             this.btnSkillEditor.Click += new System.EventHandler(this.BtnSkillEditor_Click);
             // 
             // btnShopEditor
             // 
+            this.btnShopEditor.BackColor = System.Drawing.Color.White;
+            this.btnShopEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnShopEditor.Location = new System.Drawing.Point(122, 96);
             this.btnShopEditor.Name = "btnShopEditor";
             this.btnShopEditor.Size = new System.Drawing.Size(112, 25);
             this.btnShopEditor.TabIndex = 46;
             this.btnShopEditor.Text = "Shop Editor";
-            this.btnShopEditor.UseVisualStyleBackColor = true;
+            this.btnShopEditor.UseVisualStyleBackColor = false;
             this.btnShopEditor.Click += new System.EventHandler(this.BtnShopEditor_Click);
             // 
             // btnAnimationEditor
             // 
+            this.btnAnimationEditor.BackColor = System.Drawing.Color.White;
+            this.btnAnimationEditor.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnAnimationEditor.Location = new System.Drawing.Point(4, 158);
             this.btnAnimationEditor.Name = "btnAnimationEditor";
             this.btnAnimationEditor.Size = new System.Drawing.Size(112, 25);
             this.btnAnimationEditor.TabIndex = 47;
             this.btnAnimationEditor.Text = "Animation Editor";
-            this.btnAnimationEditor.UseVisualStyleBackColor = true;
+            this.btnAnimationEditor.UseVisualStyleBackColor = false;
             this.btnAnimationEditor.Click += new System.EventHandler(this.BtnAnimationEditor_Click);
             // 
             // FrmLogin
