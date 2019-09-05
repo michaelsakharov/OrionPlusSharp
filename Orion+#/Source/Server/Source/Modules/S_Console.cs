f77d8619278e05e679213afb4750460b528b3fb7
Michael S <acer1171090@gmail.com>
2019-09-05 23:59:24 +1000

    Server: / no longer required for Commands - Also improved Help text

--- Orion+#/Source/Server/Source/Modules/S_Console.cs
+++ Orion+#/Source/Server/Source/Modules/S_Console.cs
@@ -31,32 +31,32 @@ namespace Engine
                 parts = line.Split(' '); if ((parts.Length < 1))
                     continue;
 
-                switch (parts[0].Trim().ToLower())
+                switch (parts[0].Trim().ToLower().Replace("/", ""))
                 {
-                    case "/help":
+                    case "help":
                         {
                             Console.WriteLine("/help, Shows this message.");
                             Console.WriteLine("/exit, Closes down the server.");
-                            Console.WriteLine("/setadmin, Sets player access level, use with '/setadmin playername powerlvl' powerlevel goes from 0 for player, to 4 to creator.");
-                            Console.WriteLine("/kick, Kicks user from server, use with '/kick playername'");
-                            Console.WriteLine("/ban, Bans user from server, use with '/ban playername'");
-                            Console.WriteLine("/timespeed, Set Game Speed '/timespeed speed'");
-                            Console.WriteLine("/ip, View the ip of the server '/ip'");
-                            Console.WriteLine("/say, Send a global message for everyone to see '/say message'");
-                            Console.WriteLine("/setxpmultiplier, Set the Global XP multiplier '/setxpmultiplier multiplayer'");
-                            Console.WriteLine("/debugtext, Toggle Debug Text '/debugtext'");
-                            Console.WriteLine("/unlockcps, unlock CPS '/unlockcps'");
-                            Console.WriteLine("/lockcps, lock CPS '/lockcps'");
+                            Console.WriteLine("/setadmin, Sets player access level, usage '/setadmin playername powerlvl' powerlevel goes from 0 for player, to 4 to creator.");
+                            Console.WriteLine("/kick, Kicks user from server, usage '/kick playername'");
+                            Console.WriteLine("/ban, Bans user from server, usage '/ban playername'");
+                            Console.WriteLine("/timespeed, Set Game Speed, usage  '/timespeed speed'");
+                            Console.WriteLine("/ip, View the ip of the server");
+                            Console.WriteLine("/say, Send a global message for everyone to see, usage '/say message'");
+                            Console.WriteLine("/setxpmultiplier, Set the Global XP multiplier, usage '/setxpmultiplier multiplayer'");
+                            Console.WriteLine("/debugtext, Toggle Debug Text");
+                            Console.WriteLine("/unlockcps, unlock CPS");
+                            Console.WriteLine("/lockcps, lock CPS");
                             break;
                         }
 
-                    case "/exit":
+                    case "exit":
                         {
                             S_General.DestroyServer();
                             break;
                         }
 
-                    case "/setadmin":
+                    case "setadmin":
                         {
                             if (parts.Length > 3 || parts.Length < 3)
                             {
@@ -129,7 +129,7 @@ namespace Engine
                             break;
                         }
 
-                    case "/kick":
+                    case "kick":
                         {
                             if (parts.Length > 2 || parts.Length < 2) {
                                 Console.WriteLine("Incorrect usage.");
@@ -150,7 +150,7 @@ namespace Engine
                             break;
                         }
 
-                    case "/ban":
+                    case "ban":
                         {
                             if (parts.Length > 2 || parts.Length < 2)
                             {
@@ -168,7 +168,7 @@ namespace Engine
                             break;
                         }
 
-                    case "/timespeed":
+                    case "timespeed":
                         {
                             if (parts.Length > 2 || parts.Length < 2)
                             {
@@ -183,12 +183,12 @@ namespace Engine
                             break;
                         }
 
-                    case "/ip":
+                    case "ip":
                         {
                             Console.WriteLine("Ip:" + S_General.MyIPAddress);
                             break;
                         }
-                    case "/say":
+                    case "say":
                         {
                             if (parts.Length > 2)
                             {
@@ -210,7 +210,7 @@ namespace Engine
                             Console.WriteLine(message);
                             break;
                         }
-                    case "/setxpmultiplier":
+                    case "setxpmultiplier":
                         {
                             if (parts.Length > 2 || parts.Length < 2)
                             {
@@ -222,7 +222,7 @@ namespace Engine
                             Console.WriteLine("Global XP Multiplayer set to: " + xpMultiplayer);
                             break;
                         }
-                    case "/unlockcps":
+                    case "unlockcps":
                         {
                             if (parts.Length > 1 || parts.Length < 1)
                             {
@@ -233,7 +233,7 @@ namespace Engine
                             Console.WriteLine("CPS: " + S_General.gameCPS + " CPS is Unlocked");
                             break;
                         }
-                    case "/lockcps":
+                    case "lockcps":
                         {
                             if (parts.Length > 1 || parts.Length < 1)
                             {
@@ -244,7 +244,7 @@ namespace Engine
                             Console.WriteLine("CPS: " + S_General.gameCPS + " CPS is locked");
                             break;
                         }
-                    case "/debugtext":
+                    case "debugtext":
                         {
                             if (parts.Length > 1)
                             {
@@ -262,7 +262,7 @@ namespace Engine
 
                     default:
                         {
-                            Console.WriteLine("Invalid Command. If you are unsure of the functions type '/help'.");
+                            Console.WriteLine("Invalid Command. If you are unsure of the functions type 'help'.");
                             break;
                         }
                 }
